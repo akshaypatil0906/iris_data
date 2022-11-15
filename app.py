17c021abc71999cb4cd24c70f844e0e2a43b9c8a
unknown <akshaypatil0906@gmail.com>
2022-11-15 23:35:46 +0530

    updated files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request,jsonify
+import pickle
+import numpy as np
+app = Flask(__name__)
+model = pickle.load(open("rf_clf_model.pkl", "rb"))
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+@app.route("/predict",methods= ["POST"])
+def predict():
+    ip_features = [float(x) for x in request.form.values()]
+    features = [np.array(ip_features)]
+    #features = np.append(features,[0,0,0])
+    prediction = model.predict(features)
+    #prediction.reshape(-1,1)
+    return render_template("home.html", prediction_text = "The insurance is {}".format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug = True)
