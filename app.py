96a6c20162a3cd27370f2ca03962c85ec4633650
unknown <akshaypatil0906@gmail.com>
2022-11-16 16:18:21 +0530

    New Changes

--- app.py
+++ app.py
@@ -14,8 +14,16 @@ def predict():
     features = [np.array(ip_features)]
     #features = np.append(features,[0,0,0])
     prediction = model.predict(features)
-    #prediction.reshape(-1,1)
-    return render_template("home.html", prediction_text = "The insurance is {}".format(prediction))
+    #i=[]
+    if(prediction==0):
+        i ="Setosa"
+    elif prediction == 1:
+        i = "Verginica"
+    elif prediction == 2:
+        i = "vernicolor"
+    else:
+        i = "False"
+    return render_template("home.html", prediction_text = "predicted flower is {}".format(i))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host="0.0.0.0"port=8080, debug = False)
